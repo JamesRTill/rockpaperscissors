266ceeb7555fc3faa5cecfeef36361f90e4f0a40
James Till <james_till@outlook.com>
2023-06-20 14:20:13 -0400

    Created separate class for computer opponents. Organized code.

---
+++ RockPaperScissors/Models/ComputerAIModel.cs
@@ -0,0 +1,117 @@
+﻿namespace RockPaperScissors.Models
+{
+    public class ComputerAIModel
+    {
+        public string name;
+        public int aggression;
+        public int[] bias;
+
+        private string GenerateName()
+        {
+            string[] nameList =
+            {
+                "James", "John", "Jill", "Kate", "Brad", "Todd", "Ted", "Brandy", "Alex", "Mom", "Mary", "Zero", "Kid"
+            };
+            Random rng = new Random();
+            return nameList[rng.Next(nameList.Length)];
+        }
+        private int GenerateAggression()
+        {
+            Random rng = new Random();
+            return rng.Next(3) + 1;
+        }
+        private int[] GenerateBias()
+        {
+            int[] outcome = { 0, 0, 0 };
+            Random rng = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                switch (rng.Next(3))
+                {
+                    case 0:
+                        outcome[0]++;
+                        break;
+                    case 1:
+                        outcome[1]++;
+                        break;
+                    default:
+                        outcome[2]++;
+                        break;
+                }
+            }
+            return outcome;
+        }
+        public void AdaptBias(string playerLastUsed)
+        {
+            int[] newBias = bias;
+            Random rng = new Random();
+            rng.Next(2);
+
+            switch (playerLastUsed)
+            {
+                case "rock":
+                    newBias[1] += aggression;
+                    switch (rng.Next(2))
+                    {
+                        case 0:
+                            newBias[0] -= aggression;
+                            break;
+                        case 1:
+                            newBias[2] -= aggression;
+                            break;
+                    }
+                    break;
+                case "paper":
+                    newBias[2] += aggression;
+                    switch (rng.Next(2))
+                    {
+                        case 0:
+                            newBias[0] -= aggression;
+                            break;
+                        case 1:
+                            newBias[1] -= aggression;
+                            break;
+                    }
+                    break;
+                default:
+                    newBias[0] += aggression;
+                    switch (rng.Next(2))
+                    {
+                        case 0:
+                            newBias[1] -= aggression;
+                            break;
+                        case 1:
+                            newBias[2] -= aggression;
+                            break;
+                    }
+                    break;
+            }
+            bias = newBias;
+        }
+        public string GenerateChoice()
+        {
+            Random rng = new Random();
+
+            if (rng.Next(10) < bias[0])
+            {
+                return "rock";
+            }
+            else if (rng.Next(10) < bias[0] + bias[1])
+            {
+                return "paper";
+            }
+            else
+            {
+                return "scissors";
+            }
+        }
+
+        // Constructor
+        public ComputerAIModel()
+        {
+            name = GenerateName();
+            aggression = GenerateAggression();
+            bias = GenerateBias();
+        }
+    }
+}
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,99 +1,12 @@
-﻿internal class Program
+﻿using RockPaperScissors.Models;
+
+internal class Program
 {
 
     public static void Quit()
     {
         Environment.Exit(0);
     }
-
-    public static int[] GenerateRandomBias()
-    {
-        int[] outcome = { 0, 0, 0 };
-        Random rng = new Random();
-        for (int i = 0; i < 10; i++)
-        {
-            switch (rng.Next(3))
-            {
-                case 0:
-                    outcome[0]++;
-                    break;
-                case 1:
-                    outcome[1]++;
-                    break;
-                default:
-                    outcome[2]++;
-                    break;
-            }
-        }
-        return outcome;
-    }
-
-    public static int[] AdaptComputerBias(int[] oldBias, string playerLastUsed)
-    {
-        int[] newBias = oldBias;
-        Random rng = new Random();
-        rng.Next(2);
-
-        switch (playerLastUsed)
-        {
-            case "rock":
-                newBias[1]++;
-                switch (rng.Next(2))
-                {
-                    case 0:
-                        newBias[0]--;
-                        break;
-                    case 1:
-                        newBias[2]--;
-                        break;
-                }
-                return newBias;
-            case "paper":
-                newBias[2]++;
-                switch (rng.Next(2))
-                {
-                    case 0:
-                        newBias[0]--;
-                        break;
-                    case 1:
-                        newBias[1]--;
-                        break;
-                }
-                return newBias;
-            default:
-                newBias[0]++;
-                switch (rng.Next(2))
-                {
-                    case 0:
-                        newBias[1]--;
-                        break;
-                    case 1:
-                        newBias[2]--;
-                        break;
-                }
-                return newBias;
-        }
-        
-    }
-
-    public static string GenerateComputerChoice(int[] bias)
-    {
-        Random rng = new Random();
-
-        if (rng.Next(10) < bias[0])
-        {
-            return "rock";
-        }
-        else if (rng.Next(10) < bias[0] + bias[1])
-        {
-            return "paper";
-        }
-        else
-        {
-            return "scissors";
-        }
-    }
-
     public static string ComputeOutcome(string player, string computer)
     {
         if (player == computer)
@@ -131,7 +44,7 @@
     private static void Main(string[] args)
     {
         Console.Title = "Rock paper scissors";
-        string compInput;
+        string computerInput;
         int playerWins, playerLoses, ties;
         
         // Program loop
@@ -139,17 +52,16 @@
         {
             
             playerWins = 0; playerLoses = 0; ties = 0;
-            int[] compBias = GenerateRandomBias();
+            ComputerAIModel computer = new ComputerAIModel();
 
             // Gameplay loop
             while (true)
             {
                 Console.Clear();
                 Console.WriteLine($"Wins: {playerWins}, Loses: {playerLoses}, Ties: {ties}");
-
-                // Displays the computer bias
-                Console.WriteLine($"Computer bias: Rock: {compBias[0]}, Paper: {compBias[1]}, Scissors: {compBias[2]}");
-
+                // Displays current computer bias and aggression
+                //Console.WriteLine($"Computer aggression: +{computer.aggression}, Computer bias: Rock: { computer.bias[0]}, Paper: { computer.bias[1]}, Scissors: { computer.bias[2]}");
+                Console.WriteLine($"Opponent: {computer.name}");
                 Console.WriteLine("\nChoose rock, paper, scissors, quit, or restart");
                 Console.Write("Choice: ");
                 string playerInput = Console.ReadLine();
@@ -157,7 +69,7 @@
                 {
                     Console.Clear();
                     Console.WriteLine("Please make a valid choice.");
-                    Console.WriteLine("Press any key to continue");
+                    Console.Write("Press any key to continue");
                     Console.ReadKey();
                 }
                 else if (playerInput == "quit")
@@ -171,9 +83,9 @@
                 else if (playerInput == "rock" || playerInput == "paper" || playerInput == "scissors")
                 {
                     Console.Clear();
-                    compInput = GenerateComputerChoice(compBias);
-                    Console.WriteLine($"The computer chose: {compInput}.");
-                    switch (ComputeOutcome(playerInput, compInput))
+                    computerInput = computer.GenerateChoice();
+                    Console.WriteLine($"The computer chose: {computerInput}.");
+                    switch (ComputeOutcome(playerInput, computerInput))
                     {
                         case "win":
                             playerWins++;
@@ -188,7 +100,7 @@
                             Console.Write("It's a tie.");
                             break;
                     };
-                    compBias = AdaptComputerBias(compBias, playerInput);
+                    computer.AdaptBias(playerInput);
                     Console.Write("\nType any key to continue...");
                     Console.ReadKey();
                 }
@@ -196,12 +108,10 @@
                 {
                     Console.Clear();
                     Console.WriteLine($"{playerInput} is not a valid choice.");
-                    Console.WriteLine("Press any key to continue...");
+                    Console.Write("Press any key to continue...");
                     Console.ReadKey();
                 }
-
             }
-            
         }
     }
 }
